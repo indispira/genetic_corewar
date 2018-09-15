5a1c95203af3de677d1f9894024c25f56850e232
BOULET <stephane.boulet@recast.ai>
2018-09-15 11:26:16 +0200

    add script to evaluate a single champion against stock

---
+++ test_champion.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import shlex
+import subprocess
+
+from subprocess import Popen, PIPE
+
+if len(sys.argv) == 1:
+  exit(0)
+p1 = sys.argv[1]
+score = 0
+champs = os.listdir('stock')
+print('There are', len(champs), 'champions in the stock folder.')
+for c in champs:
+  p2 = 'stock/' + c
+  cmd = './corewar ' + p1 + ' ' + p2
+  args = shlex.split(cmd)
+  res = subprocess.Popen(args, stdout=PIPE)
+  res = res.communicate()[0].decode('utf-8').splitlines()
+  if res[-1][11] == '1':
+    print(c, 'defeated.')
+    score += 1
+print('Your champion has defeat', score, 'champions from the stock.')
